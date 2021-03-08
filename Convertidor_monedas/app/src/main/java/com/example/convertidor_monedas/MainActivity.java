a77bbb1ca8a70449ac302de5f1bac06893699316
César Ricardo Lazcano Valdez <cesar.lazcano.1211@gmail.com>
2021-03-08 13:54:25 -0600

    Uso de Layouts Linear, Frame, Grid y relative

--- Convertidor_monedas/app/src/main/java/com/example/convertidor_monedas/MainActivity.java
+++ Convertidor_monedas/app/src/main/java/com/example/convertidor_monedas/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
     public void convertir(View view){
-        
+        EditText editText = findViewById(R.id.editText);
+        String pesosString = editText.getText().toString();
+        double pesos = Double.parseDouble(pesosString);
+        double dolares = pesos * 20.11;
+        String dolaresString = String.format("%.2f", dolares);
+        Toast.makeText(this, dolaresString, Toast.LENGTH_LONG).show();
     }
 
     @Override
