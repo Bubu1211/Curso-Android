83ba04ed16198e17e800b65dff37acc4b8455927
César Ricardo Lazcano Valdez <cesar.lazcano.1211@gmail.com>
2021-03-08 15:50:16 -0600

    Inclusión de Basicctivity con Material Design

---
+++ Componentes_Material_Design/app/src/main/java/com/componentes_material_design/MainActivity.java
@@ -0,0 +1,21 @@
+package com.componentes_material_design;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void cambiar(View view){
+        Intent i = new Intent(this, basicActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
